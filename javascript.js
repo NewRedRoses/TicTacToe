557875b5fd8b65aaf4b8a80652fcb20c3840ef6a
Mark A. <mark001616@gmail.com>
2023-10-13 21:00:06 -0400

    Add support for two players

--- javascript.js
+++ javascript.js
@@ -1,43 +1,74 @@
 const GameBoard = (() => {
   // let values = ["O", "X", "O", "X", "O", "X", "X", "O", "X"];
   let values = ["", "", "", "", "", "", "", "", ""];
+  let turnCount = 0;
+  let cellId;
+
+  const player1 = playerFactory("John", "X");
+  const player2 = playerFactory("Luke", "O");
+
+  //  maybe try changing it to be more specific instead of listening for the whole container
+  const element = document.querySelector(".grid-container");
+  element.addEventListener("click", (event) => getIdOnClick(event));
+
+  function playerFactory(name, marker) {
+    const getMarker = () => marker;
+    const getName = () => name;
+    const markOnClick = () => {
+      if (checkIfValidSelection()) values[cellId] = getMarker();
+    };
+
+    return { getName, getMarker, markOnClick };
+  }
+
   const display = function () {
     values.forEach((item, index) => {
       const cell = document.getElementById(`${index}`);
       cell.innerHTML = item;
     });
   };
-  return { display,values};
+  const checkIfValidSelection = (id) => {
+    console.log("clicked id: ", values[id]);
+    if (values[id] != "X" || values[id] != "O") return true;
+    else return false;
+  };
+
+  const getIdOnClick = function (event) {
+    // Need to use this thing since I'm using a callback
+    const clickedItemClass = event.target.getAttribute("class");
+    if (clickedItemClass === "cell") {
+      // console.log(event.target);
+      cellId = event.target.getAttribute("id");
+      console.log(values);
+      checkRound();
+    }
+  };
+  const resetListener = () => {
+    const element = document.querySelector(".grid-container");
+    element.removeEventListener("click", (event) =>
+      getIdOnClick(event, player)
+    );
+  };
+  const checkRound = () => {
+    // console.log(turnCount);
+    if (turnCount % 2 == 0) {
+      player1.markOnClick();
+    } else {
+      player2.markOnClick();
+    }
+    turnCount++;
+    display();
+  };
+  const start = () => {
+    display();
+  };
+  return {
+    start,
+    checkRound,
+    display,
+    values,
+    checkIfValidSelection,
+  };
 })();
 
-GameBoard.display();
-function playerFactory(name, marker) {
-  const getMarker = () => marker;
-  let cell;
-  const getCellId = function (callback) {
-    const element = document.querySelector(".grid-container");
-    let selectedCell;
-    element.addEventListener("click", (event) => {
-      const clickedItemClass = event.target.getAttribute("class");
-      // Ingnore whitespace between cells
-      if (clickedItemClass === "cell") {
-        cell = event.target.getAttribute("id");
-        callback(cell);
-      }
-    });
-  };
-  const setMarkerOnClick = function () {
-    // Need to use this thing since im using a callback
-    getCellId(function (id) {
-      cell = id;
-      GameBoard.values[id] = marker;
-      GameBoard.display();
-    });
-  };
-  return { setMarkerOnClick };
-}
-// Temp objects
-const player1 = playerFactory("John", "X");
-const player2 = playerFactory("Luke", "O");
-
-player2.setMarkerOnClick();
+GameBoard.start();
